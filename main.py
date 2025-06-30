ec321f4853fb22ce7ec77e5e62f60cd7a56ce92a
Abdul Samad <abdulsamad97040@gmail.com>
2025-06-30 13:20:55 +0530

    Separated index Landamark and added moving of mouse using index finger landmark

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 import cv2
 import mediapipe as mp
+import pyautogui as pg
 cap= cv2.VideoCapture(0)
 hand_detection = mp.solutions.hands.Hands()
 drawing_points = mp.solutions.drawing_utils
+screen_width, screen_height = pg.size()
+frame_margin=100# Get the screen width and height
 while True:
     ret, frame = cap.read()
+    #undoing the mirror effect of the webcam
+    frame=cv2.flip(frame,1)
+    frame_height, frame_width = frame.shape[:2]
+
+    
     rgb_frame=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output=hand_detection.process(rgb_frame)
     hands= output.multi_hand_landmarks
     if hands:
         for hand in hands:
             drawing_points.draw_landmarks(frame, hand)
-            #for marking indices beside the points on the hand
-            # Uncomment the following lines if you want to display indices next to landmarks
-            # This can be useful for debugging or understanding the landmark positions
-            # 
-            # Note: This will add text labels to each landmark, which may clutter the view.
-            # If you want to see the indices, you can uncomment these lines.
-            # for idx, landmark in enumerate(hand.landmark):
-            #     h, w, _ = frame.shape
-            #     cx, cy = int(landmark.x * w), int(landmark.y * h)
-            #     cv2.putText(frame, str(idx), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 
-            #         0.5, (0, 255, 0), 1, cv2.LINE_AA)
-            
+            landmark=hand.landmark
+            active_width = frame_width -2 * frame_margin
+            active_height = frame_height - 2 * frame_margin
+            for idx, landmark in enumerate(landmark):
+                x=int(landmark.x*frame_width)
+                y=int(landmark.y*frame_height)
+                print(x,y)
+                
+                
+                if idx==8:
+                    cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 0), thickness=1)
+                    
+                    index_x=screen_width/frame_width*x   # Convert to screen coordinates
+                    index_y=screen_height/frame_height*y
+                    pg.moveTo(index_x, index_y)
+                    # pg.moveTo(x, y)
+                    
+                
+            landmarks=hand.landmark
     # print(hands)
     cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == 27:  # Press ESC to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
